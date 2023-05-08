3f71cdad65bb9c5271aa6d04f75d05ca43130aa0
emininmac <mamihaha4@gmail.com>
2023-05-08 19:31:02 +0300

    Initial Commit

---
+++ MerdalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MerdalApp
+//
+//  Created by Muhammed Emin Bardakcı on 8.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
